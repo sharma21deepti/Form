b0a91b7095a4bae41c5cd36f1eb5ed2e930b5e04
Deepti Sharma <91265103+sharma21deepti@users.noreply.github.com>
2023-11-07 01:54:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Display from "./component/Display"
+
+function App() {
+  
+  return (
+    <Display />
+  );
+  
+}
+
+export default App;
